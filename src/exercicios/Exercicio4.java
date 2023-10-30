b9234093d3cfa92cf3deaf0b644613b44235713f
Fabiana Alves <biazita.santo@hotmail.com>
2023-10-30 07:18:09 -0300

    Exercício 4

---
+++ src/exercicios/Exercicio4.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        int idade = sc.nextInt();
+        int soma = 0;
+        int cont = 0;
+
+        while (idade >= 0) {
+            soma = soma + idade;
+            cont = cont +1;
+            idade = sc.nextInt();
+        }
+        if (cont > 0) {
+            double media = (double) soma / cont;
+            System.out.printf("%.2f%n", media);
+        }
+        else {
+            System.out.println("Impossível calcular");
+        }
+        sc.close();
+    }
+}
